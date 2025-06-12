8c294b30ef9496c731a1337f1d091a1a07e31eaf
nailul.firdaus <Nailul.Firdaus@bni.co.id>
2025-06-12 21:37:04 +0700

    refactor: change schema to change users. add categories, types. delete skill tools

--- drizzle/0000_fantastic_proudstar.sql
+++ drizzle/0000_fantastic_proudstar.sql
@@ -1,7 +1,6 @@
-CREATE TABLE "skill_tools" (
+CREATE TABLE "categories" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"skill_id" integer,
-	"tool_id" integer,
+	"name" varchar(100) NOT NULL,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
@@ -9,8 +8,7 @@ CREATE TABLE "skill_tools" (
 CREATE TABLE "skills" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"user_id" integer,
-	"name" varchar(255) NOT NULL,
-	"icon_url" text NOT NULL,
+	"tool_id" integer,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
@@ -32,12 +30,22 @@ CREATE TABLE "tools" (
 	"updated_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
+CREATE TABLE "types" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
+--> statement-breakpoint
 CREATE TABLE "users" (
 	"id" serial PRIMARY KEY NOT NULL,
+	"photo_url" text NOT NULL,
+	"email" varchar(255) NOT NULL,
 	"name" varchar(255) NOT NULL,
+	"phone" varchar(18) NOT NULL,
 	"job" varchar(255) NOT NULL,
-	"about" varchar(255) NOT NULL,
-	"story" varchar(255) NOT NULL,
+	"about" text NOT NULL,
+	"story" text NOT NULL,
 	"interest" varchar(100) NOT NULL,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
@@ -57,14 +65,17 @@ CREATE TABLE "works" (
 	"title" varchar(255) NOT NULL,
 	"description" varchar(255) NOT NULL,
 	"image_url" text NOT NULL,
+	"category_id" integer,
+	"type_id" integer,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
-ALTER TABLE "skill_tools" ADD CONSTRAINT "skill_tools_skill_id_skills_id_fk" FOREIGN KEY ("skill_id") REFERENCES "public"."skills"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "skill_tools" ADD CONSTRAINT "skill_tools_tool_id_tools_id_fk" FOREIGN KEY ("tool_id") REFERENCES "public"."tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "skills" ADD CONSTRAINT "skills_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "skills" ADD CONSTRAINT "skills_tool_id_tools_id_fk" FOREIGN KEY ("tool_id") REFERENCES "public"."tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "socials" ADD CONSTRAINT "socials_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "work_tools" ADD CONSTRAINT "work_tools_work_id_works_id_fk" FOREIGN KEY ("work_id") REFERENCES "public"."works"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "work_tools" ADD CONSTRAINT "work_tools_tool_id_tools_id_fk" FOREIGN KEY ("tool_id") REFERENCES "public"."tools"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "works" ADD CONSTRAINT "works_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "works" ADD CONSTRAINT "works_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "works" ADD CONSTRAINT "works_category_id_categories_id_fk" FOREIGN KEY ("category_id") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "works" ADD CONSTRAINT "works_type_id_types_id_fk" FOREIGN KEY ("type_id") REFERENCES "public"."types"("id") ON DELETE no action ON UPDATE no action;
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -9,10 +9,13 @@ import {
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
+  photoUrl: text("photo_url").notNull(),
+  email: varchar("email", { length: 255 }).notNull(),
   name: varchar("name", { length: 255 }).notNull(),
+  phone: varchar("phone", { length: 18 }).notNull(),
   job: varchar("job", { length: 255 }).notNull(),
-  about: varchar("about", { length: 255 }).notNull(),
-  story: varchar("story", { length: 255 }).notNull(),
+  about: text("about").notNull(),
+  story: text("story").notNull(),
   interest: varchar("interest", { length: 100 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
@@ -45,6 +48,8 @@ export const works = pgTable("works", {
   title: varchar("title", { length: 255 }).notNull(),
   description: varchar("description", { length: 255 }).notNull(),
   imageUrl: text("image_url").notNull(),
+  categoryId: integer("category_id").references(() => categories.id),
+  typeId: integer("type_id").references(() => types.id),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
@@ -66,20 +71,23 @@ export const skills = pgTable("skills", {
   userId: integer("user_id").references(() => users.id, {
     onDelete: "cascade",
   }),
-  name: varchar("name", { length: 255 }).notNull(),
-  iconUrl: text("icon_url").notNull(),
+  toolId: integer("tool_id").references(() => tools.id, {
+    onDelete: "cascade",
+  }),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
 
-export const skillTools = pgTable("skill_tools", {
+export const categories = pgTable("categories", {
   id: serial("id").primaryKey(),
-  skillId: integer("skill_id").references(() => skills.id, {
-    onDelete: "cascade",
-  }),
-  toolId: integer("tool_id").references(() => tools.id, {
-    onDelete: "cascade",
-  }),
+  name: varchar("name", { length: 100 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow(),
+});
+
+export const types = pgTable("types", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
